9e2edea7c92a3bd432de2def4ff05737e73abc31
Sahil <Sahil@sahils-mbp.home>
2019-09-26 01:04:31 -0400

    Patel3s7_Assingnment04 Due 9/26/19

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,5 +1,11 @@
 /*
+ *Assignment 04
  * Sahil Patel
+ * Pyramids with loops
+ * 9/26/19
+ * Course: App Dev W/ Java
+ * patel3s7@mail.uc.edu
+ * 
  * Printing pyramids using loops. It kind-of works. 
  * What I really want is
         1
@@ -13,12 +19,31 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
-			}
-			System.out.println("");
+		
+		int n = 5;
+		int LineN;
+		
+		//outer loop to do number of rows
+		for (int i=0; i<n; i++)
+		{	
+		//inner loop to do spaces
+		for (int j=n-i; j>1; j--)
+		{
+			System.out.print(" ");
 		}
+		
+		//inner loop to do number of columns
+		LineN= i+1;
+		System.out.print(LineN);
+		for (int j=1; j<=i; j++)
+		{
+		//printing numbers
+		System.out.print(LineN + "" + LineN);
+	}
+		//ending lines after the rows
+		System.out.println();
 	}
 }
+}
+
+	
